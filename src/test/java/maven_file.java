3f20df99e592eb0d523f48c217a1eced7d25f384
devipriyaarumugam <devipriya0906@gmail.com>
2017-02-06 23:32:24 +0530

    second commit

--- src/test/java/maven_file.java
+++ src/test/java/maven_file.java
@@ -3,6 +3,7 @@ public class maven_file {
 
 	public static void main(String[] args) {
 		System.out.println("This is a maven test project");
+		System.out.println("This the second maven commit");
 
 	}
 
